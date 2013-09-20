c38f7a8c3bc95d653cbb2437ceab3b6d1e2f25ea
Vitaliy Zakharov <zakharovvi@gmail.com>
2013-09-20 21:00:30 +0600

    fix cs

--- LSS/Crypt.php
+++ LSS/Crypt.php
@@ -2,21 +2,21 @@
 /**
  *  OpenLSS - Lighter Smarter Simpler
  *
- *	This file is part of OpenLSS.
+ *    This file is part of OpenLSS.
  *
- *	OpenLSS is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU Lesser General Public License as
- *	published by the Free Software Foundation, either version 3 of
- *	the License, or (at your option) any later version.
+ *    OpenLSS is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as
+ *    published by the Free Software Foundation, either version 3 of
+ *    the License, or (at your option) any later version.
  *
- *	OpenLSS is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU Lesser General Public License for more details.
+ *    OpenLSS is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
  *
- *	You should have received a copy of the 
- *	GNU Lesser General Public License along with OpenLSS.
- *	If not, see <http://www.gnu.org/licenses/>.
+ *    You should have received a copy of the
+ *    GNU Lesser General Public License along with OpenLSS.
+ *    If not, see <http://www.gnu.org/licenses/>.
  */
 namespace LSS;
 
@@ -26,129 +26,158 @@ namespace LSS;
 //	on the fly
 //---------------------------------------------------------
 
-class Crypt {
+class Crypt
+{
 
-	//NOTE: changing these would require new keys
-	static $crypt_cipher	=	MCRYPT_RIJNDAEL_256;
-	static $crypt_mode		=	MCRYPT_MODE_CBC;
-	static $crypt_rand		=	MCRYPT_DEV_URANDOM;
+    //NOTE: changing these would require new keys
+    static $crypt_cipher = MCRYPT_RIJNDAEL_256;
+    static $crypt_mode = MCRYPT_MODE_CBC;
+    static $crypt_rand = MCRYPT_DEV_URANDOM;
 
-	private $key;
-	private $iv;
+    private $key;
+    private $iv;
 
-	protected $verified = false;
+    protected $verified = false;
 
-	//-----------------------------------------------------
-	//Static Access
-	//-----------------------------------------------------
+    //-----------------------------------------------------
+    //Static Access
+    //-----------------------------------------------------
 
-	//generate usable IV for config
-	public static function IVCreate(){
-		return base64_encode(mcrypt_create_iv(
-			 mcrypt_get_iv_size(self::$crypt_cipher,self::$crypt_mode)
-			,self::$crypt_rand
-		));
-	}
+    //generate usable IV for config
+    public static function IVCreate()
+    {
+        return base64_encode(
+            mcrypt_create_iv(
+                mcrypt_get_iv_size(self::$crypt_cipher, self::$crypt_mode)
+                ,
+                self::$crypt_rand
+            )
+        );
+    }
 
-	//generate usable key for config
-	public static function keyCreate(){
-		return base64_encode(mcrypt_create_iv(
-			 mcrypt_get_key_size(self::$crypt_cipher,self::$crypt_mode)
-			,self::$crypt_rand
-		));
-	}
+    //generate usable key for config
+    public static function keyCreate()
+    {
+        return base64_encode(
+            mcrypt_create_iv(
+                mcrypt_get_key_size(self::$crypt_cipher, self::$crypt_mode)
+                ,
+                self::$crypt_rand
+            )
+        );
+    }
 
-	//-----------------------------------------------------
-	//Object Methods
-	//-----------------------------------------------------
+    //-----------------------------------------------------
+    //Object Methods
+    //-----------------------------------------------------
 
-	//setup and store keys
-	public function __construct($key,$iv){
-        if(!extension_loaded('mcrypt')) throw new \Exception('MCrypt extension not present!');
-		$this->key = $key;
-		$this->iv = $iv;
-	}
+    //setup and store keys
+    public function __construct($key, $iv)
+    {
+        if (!extension_loaded('mcrypt')) {
+            throw new \Exception('MCrypt extension not present!');
+        }
+        $this->key = $key;
+        $this->iv = $iv;
+    }
 
-	public function setKey($key){
-		$this->key = $key;
-		return $this;
-	}
+    public function setKey($key)
+    {
+        $this->key = $key;
+        return $this;
+    }
 
-	public function setIV($iv){
-		$this->iv = $iv;
-		return $this;
-	}
+    public function setIV($iv)
+    {
+        $this->iv = $iv;
+        return $this;
+    }
 
-	public function verify(){
-		$this->verifyKey();
-		$this->verifyIV();
-		$this->verified = true;
-		return $this;
-	}
+    public function verify()
+    {
+        $this->verifyKey();
+        $this->verifyIV();
+        $this->verified = true;
+        return $this;
+    }
 
-	//verify existence and size of key
-	protected function verifyKey(){
-		$key_size = mcrypt_get_key_size(self::$crypt_cipher,self::$crypt_mode);
-		//verify we have a key before starting
-		if(!isset($this->key) || is_null($this->key)){
-			throw new \Exception('No encryption key defined in config');
-		}
-		if(strlen(base64_decode($this->key)) < $key_size){
-			throw new \Exception('Encryption key is too short, required length: '.$key_size);
-		}
-		return true;
-	}
+    //verify existence and size of key
+    protected function verifyKey()
+    {
+        $key_size = mcrypt_get_key_size(self::$crypt_cipher, self::$crypt_mode);
+        //verify we have a key before starting
+        if (!isset($this->key) || is_null($this->key)) {
+            throw new \Exception('No encryption key defined in config');
+        }
+        if (strlen(base64_decode($this->key)) < $key_size) {
+            throw new \Exception('Encryption key is too short, required length: ' . $key_size);
+        }
+        return true;
+    }
 
-	//verify existencve and size of iv
-	protected function verifyIV(){
-		$iv_size = mcrypt_get_iv_size(self::$crypt_cipher,self::$crypt_mode);
-		//verify we have a key before starting
-		if(!isset($this->iv) || is_null($this->iv)){
-			throw new \Exception('No IV key defined in config');
-		}
-		if(strlen(base64_decode($this->iv)) < $iv_size){
-			throw new \Exception('Encryption IV is too shorted, required length: '.$iv_size);
-		}
-		return true;
-	}
+    //verify existencve and size of iv
+    protected function verifyIV()
+    {
+        $iv_size = mcrypt_get_iv_size(self::$crypt_cipher, self::$crypt_mode);
+        //verify we have a key before starting
+        if (!isset($this->iv) || is_null($this->iv)) {
+            throw new \Exception('No IV key defined in config');
+        }
+        if (strlen(base64_decode($this->iv)) < $iv_size) {
+            throw new \Exception('Encryption IV is too shorted, required length: ' . $iv_size);
+        }
+        return true;
+    }
 
-	//encrypt string and optionally base64_encode
-	public function encrypt($plain_string,$base64_encode=true){
-		if(!$this->verified) $this->verify();
-		//add size atom to encrypted string
-		$size = pack('N',strlen($plain_string));
-		$plain_string = $size.$plain_string;
-		//encrypt and return
-		$enc_string = mcrypt_encrypt(
-			 self::$crypt_cipher
-			,base64_decode($this->key)
-			,$plain_string
-			,self::$crypt_mode
-			,base64_decode($this->iv)
-		);
-		if($base64_encode) return base64_encode($enc_string);
-		return $enc_string;
-	}
+    //encrypt string and optionally base64_encode
+    public function encrypt($plain_string, $base64_encode = true)
+    {
+        if (!$this->verified) {
+            $this->verify();
+        }
+        //add size atom to encrypted string
+        $size = pack('N', strlen($plain_string));
+        $plain_string = $size . $plain_string;
+        //encrypt and return
+        $enc_string = mcrypt_encrypt(
+            self::$crypt_cipher,
+            base64_decode($this->key),
+            $plain_string,
+            self::$crypt_mode,
+            base64_decode($this->iv)
+        );
+        if ($base64_encode) {
+            return base64_encode($enc_string);
+        }
+        return $enc_string;
+    }
 
-	//decrypt string from an optionally base64_encoded source
-	function decrypt($enc_string,$base64_decode=true){
-		if(is_null($enc_string) || empty($enc_string)) return NULL;
-		if(!$this->verified) $this->verify();
-		//decrypt and return
-		if($base64_decode) $enc_string = base64_decode($enc_string);
-		$plain_string = mcrypt_decrypt(
-			 self::$crypt_cipher
-			,base64_decode($this->key)
-			,$enc_string
-			,self::$crypt_mode
-			,base64_decode($this->iv)
-		);
-		//get size of string from atom
-		$size = unpack('N',substr($plain_string,0,4));
-		$size = array_shift($size);
-		//make sure we have the original string
-		$plain_string = substr($plain_string,4,$size);
-		return $plain_string;
-	}
+    //decrypt string from an optionally base64_encoded source
+    function decrypt($enc_string, $base64_decode = true)
+    {
+        if (is_null($enc_string) || empty($enc_string)) {
+            return NULL;
+        }
+        if (!$this->verified) {
+            $this->verify();
+        }
+        //decrypt and return
+        if ($base64_decode) {
+            $enc_string = base64_decode($enc_string);
+        }
+        $plain_string = mcrypt_decrypt(
+            self::$crypt_cipher,
+            base64_decode($this->key),
+            $enc_string,
+            self::$crypt_mode,
+            base64_decode($this->iv)
+        );
+        //get size of string from atom
+        $size = unpack('N', substr($plain_string, 0, 4));
+        $size = array_shift($size);
+        //make sure we have the original string
+        $plain_string = substr($plain_string, 4, $size);
+        return $plain_string;
+    }
 
 }
--- test/cryptTest.php
+++ test/cryptTest.php
@@ -2,66 +2,74 @@
 /**
  *  OpenLSS - Lighter Smarter Simpler
  *
- *	This file is part of OpenLSS.
+ *    This file is part of OpenLSS.
  *
- *	OpenLSS is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU Lesser General Public License as
- *	published by the Free Software Foundation, either version 3 of
- *	the License, or (at your option) any later version.
+ *    OpenLSS is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as
+ *    published by the Free Software Foundation, either version 3 of
+ *    the License, or (at your option) any later version.
  *
- *	OpenLSS is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU Lesser General Public License for more details.
+ *    OpenLSS is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
  *
- *	You should have received a copy of the 
- *	GNU Lesser General Public License along with OpenLSS.
- *	If not, see <http://www.gnu.org/licenses/>.
+ *    You should have received a copy of the
+ *    GNU Lesser General Public License along with OpenLSS.
+ *    If not, see <http://www.gnu.org/licenses/>.
  */
-require_once(dirname(__DIR__).'/vendor/autoload.php');
+require_once(dirname(__DIR__) . '/vendor/autoload.php');
 
 use \LSS\Crypt;
 
-define('CRYPT_KEY','fDCb/LbbHHktg07zvZgLniX5Bf4gpmLv1LkM6nLuPNk=');
-define('CRYPT_IV','mhKxiKdTSD4BKR26L/QHSnaCk/1xt1vQVD1xxE37Mew=');
-define('TEST_STRING','this is the test string to encrypt');
+define('CRYPT_KEY', 'fDCb/LbbHHktg07zvZgLniX5Bf4gpmLv1LkM6nLuPNk=');
+define('CRYPT_IV', 'mhKxiKdTSD4BKR26L/QHSnaCk/1xt1vQVD1xxE37Mew=');
+define('TEST_STRING', 'this is the test string to encrypt');
 
-class CryptTest extends PHPUNIT_Framework_TestCase {
+class CryptTest extends PHPUNIT_Framework_TestCase
+{
 
-	static $crypted_string = null;
+    static $crypted_string = null;
 
-	protected $crypt = false;
+    protected $crypt = false;
 
-	protected function setUp(){
-		$this->crypt = new Crypt(CRYPT_KEY,CRYPT_IV);
-		self::$crypted_string = $this->crypt->encrypt(TEST_STRING);
-	}
+    protected function setUp()
+    {
+        $this->crypt = new Crypt(CRYPT_KEY, CRYPT_IV);
+        self::$crypted_string = $this->crypt->encrypt(TEST_STRING);
+    }
 
-	protected function tearDown(){
-		$this->crypt = false;
-		self::$crypted_string = null;
-	}
+    protected function tearDown()
+    {
+        $this->crypt = false;
+        self::$crypted_string = null;
+    }
 
-	public function testVerify(){
-		$this->assertTrue(is_object($this->crypt->verify()));
-	}
+    public function testVerify()
+    {
+        $this->assertTrue(is_object($this->crypt->verify()));
+    }
 
-	public function testEncrypt(){
-		$this->assertEquals(self::$crypted_string,$this->crypt->encrypt(TEST_STRING));
-	}
+    public function testEncrypt()
+    {
+        $this->assertEquals(self::$crypted_string, $this->crypt->encrypt(TEST_STRING));
+    }
 
-	public function testDecrypt(){
-		$this->assertEquals(TEST_STRING,$this->crypt->decrypt(self::$crypted_string));
-	}
+    public function testDecrypt()
+    {
+        $this->assertEquals(TEST_STRING, $this->crypt->decrypt(self::$crypted_string));
+    }
 
-	public function testEncryptWOBase64(){
-		self::$crypted_string = base64_decode(self::$crypted_string);
-		$this->assertEquals(self::$crypted_string,$this->crypt->encrypt(TEST_STRING,false));
-	}
+    public function testEncryptWOBase64()
+    {
+        self::$crypted_string = base64_decode(self::$crypted_string);
+        $this->assertEquals(self::$crypted_string, $this->crypt->encrypt(TEST_STRING, false));
+    }
 
-	public function testDecryptWOBase64(){
-		self::$crypted_string = base64_decode(self::$crypted_string);
-		$this->assertEquals(TEST_STRING,$this->crypt->decrypt(self::$crypted_string,false));
-	}
+    public function testDecryptWOBase64()
+    {
+        self::$crypted_string = base64_decode(self::$crypted_string);
+        $this->assertEquals(TEST_STRING, $this->crypt->decrypt(self::$crypted_string, false));
+    }
 
 }
